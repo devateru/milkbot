bb5b7278528d06908c73cc7ab4bff0794059f07d
paste/ <77318944+devateru@users.noreply.github.com>
2026-09-07 23:26:07 +0900

    embed test #4.7

--- bot.py
+++ bot.py
@@ -110,9 +110,7 @@ async def foo(interaction: discord.Interaction) -> None:
         url=r"https://arcade-songs.zetaraku.dev/maimai/song/?id=%E7%B3%BB%E3%81%8E%E3%81%A6",
         color=0xba67f8,
     )
-    embed.set_image(url="https://dp4p6x0xfi5o9.cloudfront.net/maimai/img/cover/3a914643f53b41ab5b3f0be0bb501b895a52660f8c78f8317a87a7669efc7930.png")
-    embed.set_footer(text="Re:MASTER 15.0")
-
+    embed.set_thumbnail(url="https://dp4p6x0xfi5o9.cloudfront.net/maimai/img/cover/3a914643f53b41ab5b3f0be0bb501b895a52660f8c78f8317a87a7669efc7930.png")
     view = discord.ui.View()
 
     view.add_item(
